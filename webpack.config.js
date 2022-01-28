f0c0c4fca168f42f735da80c3d910c9f8239347d
SenorGato <senoraraton@gmail.com>
2022-01-27 16:04:37 -0800

    Webpack init

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require( 'path' );
+
+module.exports = {
+
+    //Bundling mode
+    mode: 'production',
+
+    //Entry files
+    entry: './src/perlin.ts',
+
+    //Output bundles
+    output: {
+        path: path.resolve( __dirname, 'dist' ),
+        filename: 'index.js',
+    },
+
+    //File resolutions
+    resolve: {
+        extensions: [ '.ts', '.js' ],
+    },
+
+    //Loaders
+    module: {
+        rules: [
+            {
+                test: /\.tsx?/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            }
+        ]
+    }
+};
