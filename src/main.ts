fd7a13d7714d512ea8484d0445e72c1ab9ce11ef
SenorGato <senoraraton@gmail.com>
2022-01-30 21:14:08 -0800

    Flocka

--- src/main.ts
+++ src/main.ts
@@ -20,9 +20,11 @@ for( let x=0; x < (gameMapWidth/tileSize); x++) {
     for( let y=0; y< (gameMapHeight/tileSize); y++) {
         let height = Math.floor(Noise2D(x*frequency,y*frequency) * 10);
         gameMap[x][y] = new Tile(height,Terrain.Ocean,"tile");
+        console.log(gameMap[x][y].height);
         let ctx = canvas.getContext("2d");
         ctx!.beginPath();
-        ctx!.rect(x*tileSize,y*tileSize,x*tileSize+tileSize,y*tileSize+tileSize);
+        ctx!.fillStyle = `rgb(${gameMap[x][y].height},120,120)`;
+        ctx!.fillRect(x*tileSize,y*tileSize,x*tileSize+tileSize,y*tileSize+tileSize);
         ctx!.stroke();
     }
 }
