483a03e942ea69a98353094b8204e989bdaacbc9
SenorGato <senoraraton@gmail.com>
2022-01-31 09:58:07 -0800

    Its ALIVE!

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { Tile } from "./tile";
 import { Terrain } from "./enums"
 import { Noise2D } from "./perlin"
 
-let tileSize = 20;
+let tileSize = 5;
 let gameMapWidth = 800;
 let gameMapHeight = 600;
 
@@ -18,12 +18,19 @@ let frequency = .05
 for( let x=0; x < (gameMapWidth/tileSize); x++) {
     gameMap[x] = [];
     for( let y=0; y< (gameMapHeight/tileSize); y++) {
-        let height = Math.floor(Noise2D(x*frequency,y*frequency) * 10);
+        let height = Math.floor(Noise2D(x*frequency,y*frequency) * 10 + 10);
         gameMap[x][y] = new Tile(height,Terrain.Ocean,"tile");
+        
+
         console.log(gameMap[x][y].height);
         let ctx = canvas.getContext("2d");
         ctx!.beginPath();
-        ctx!.fillStyle = `rgb(${gameMap[x][y].height},120,120)`;
+        if (height < 10) {
+            ctx!.fillStyle = 'blue';
+        } else {
+            ctx!.fillStyle = 'green';
+        }
+//        ctx!.fillStyle = `rgb(${height * 10},${height * 10},${height * 10})`;
         ctx!.fillRect(x*tileSize,y*tileSize,x*tileSize+tileSize,y*tileSize+tileSize);
         ctx!.stroke();
     }
